93ed3df437ca788776507887eeed0fa474f28c5e
AlAminMiah4274 <alaminmiah4274@gmail.com>
2025-01-11 15:58:17 +0600

    urls bug has been fixed

--- task_management/tasks/urls.py
+++ task_management/tasks/urls.py
@@ -3,6 +3,6 @@ from tasks.views import contact, show_task, show_specific_task
 
 urlpatterns = [
 	path("contact/", contact),
-	path("show_task/", show_task)
+	path("show_task/", show_task),
 	path("show_task/<int:id>", show_specific_task),
 ]
